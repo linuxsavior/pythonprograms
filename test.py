ede066705bb49446fd869a807ecd83e7c0bf666c
linuxsavior <linuxsavior@gmail.com>
2024-03-03 21:28:33 -0500

    updates

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
 import os
+import requests
 print ("Hello")
 print ("Good to know")
 print ("Leo the badass")
 print ("Hola, Como Estas")
+
+
+def main():
+    for x in range(1,5):
+        print (x)
+    
+    url = "https://google.com"
+    r = requests.get(url)
+    print(r.status_code)
+    print(r.headers)
+    print(r.is_redirect)
+    print (r.url)
+
+
+
+
+if __name__ == "__main__":
+    main()
